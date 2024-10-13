43a0c71e6d2cc163a518cd1576a3f01a95881a83
jinhyeon-dev <jinhyeon.dev@gmail.com>
2024-10-13 23:52:12 +0900

    first commit

--- lib/component/colors.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: non_constant_identifier_names
-
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color BACKGROUNDCOLOR = Colors.white;
-  static Color MAINCOLOR = Colors.blue;
-  static Color TEXTCOLOR = Colors.black;
-}
